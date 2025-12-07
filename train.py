b256b9ae294fa062a9973af03846b27973e15594
夏煜 <xianull0418@gmail.com>
2025-12-07 12:56:15 +0800

    add wandb

--- train.py
+++ train.py
@@ -1,8 +1,8 @@
 import hydra
-from omegaconf import DictConfig
+from omegaconf import DictConfig, OmegaConf
 import lightning.pytorch as pl
 from lightning.pytorch.callbacks import RichProgressBar
-
+import wandb
 
 @hydra.main(version_base=None, config_path="conf", config_name="config")
 def main(cfg: DictConfig):
@@ -15,17 +15,25 @@ def main(cfg: DictConfig):
     # 2. 实例化 Model
     print(f"初始化模型: {cfg.model._target_} (LR: {cfg.model.learning_rate})")
     model = hydra.utils.instantiate(cfg.model)
-
+    logger = None
+    if "logger" in cfg and cfg.logger:
+        print(f"初始化 Logger: {cfg.logger._target_}")
+        logger = hydra.utils.instantiate(cfg.logger)
+        
+        # 技巧：把 Hydra 的完整配置传给 logger，这样 WandB 网页上能看到所有参数
+        # 注意：WandbLogger 会自动记录 model.hparams，这里是额外补充其他配置(如 trainer, data)
+        logger.experiment.config.update(OmegaConf.to_container(cfg, resolve=True))
     # 3. 实例化 Trainer
     # 这里我们手动加一个 RichProgressBar 让控制台输出好看点
     print("初始化 Trainer...")
     trainer = hydra.utils.instantiate(
         cfg.train, 
+        logger=logger,
         callbacks=[RichProgressBar()]
     )
 
     # 4. 开始训练
     trainer.fit(model, datamodule=dm)
-
+    wandb.finish()
 if __name__ == "__main__":
     main()
